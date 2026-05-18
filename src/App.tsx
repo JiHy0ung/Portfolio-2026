e92914c7a6611db4474bfd7c94b28ddbe028ed72
JiHyoung You <fbwlgud51@gmail.com>
2026-05-18 20:00:56 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen flex justify-center items-center ">
+      <h1 className="font-syne text-7xl font-black">Portfolio</h1>
+    </div>
+  );
+}
+
+export default App;
