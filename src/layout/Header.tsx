214064ce5265bc7fd48a239027af880bc9a64d76
JiHyoung You <fbwlgud51@gmail.com>
2026-05-19 08:30:27 +0900

    feat: 히어로 섹션 추가

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,13 +1,13 @@
 const Header = () => {
   return (
-    <div className="w-full flex justify-between items-center px-[1.5rem] py-[0.75rem]">
+    <div className="w-full flex justify-between items-center px-[1.5rem] py-[0.75rem] fixed backdrop-blur-xs">
       <h1 className="font-syne text-1xl font-black cursor-pointer">
         JH<span className="text-red-600">.</span>
       </h1>
 
-      <div className="font-syne">
+      {/* <div className="font-syne">
         <button className="cursor-pointer">menu</button>
-      </div>
+      </div> */}
     </div>
   );
 };
