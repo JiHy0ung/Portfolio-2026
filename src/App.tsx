2751002a004d55a60b10d7ac6aca073a2320a1ae
JiHyoung You <fbwlgud51@gmail.com>
2026-05-19 13:43:35 +0900

    feat: 푸터 추가

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Hero from "./components/sections/Hero";
 import Projects from "./components/sections/Projects";
 import Skills from "./components/sections/Skills";
 import CustomCursor from "./components/ui/CustomCursor";
+import Footer from "./layout/Footer";
 import Header from "./layout/Header";
 
 function App() {
@@ -42,6 +43,8 @@ function App() {
         <Projects />
 
         <Contact />
+
+        <Footer />
 
         <ScrollToTop />
       </main>
---
+++ src/layout/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <div className="mb-[2rem]">
+      <p className="font-syne text-red-600/60">
+        © 2026 yjh Portfolio. All rights reserved.
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
