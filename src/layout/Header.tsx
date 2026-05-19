1c0681e358c85fd0c17289b1aebb40f429d8078a
JiHyoung You <fbwlgud51@gmail.com>
2026-05-19 10:41:49 +0900

    design: 헤더 zindex 수정

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,6 +1,6 @@
 const Header = () => {
   return (
-    <div className="w-full flex justify-between items-center px-[1.5rem] py-[0.75rem] fixed backdrop-blur-xs">
+    <div className="z-[9989] w-full flex justify-between items-center px-[1.5rem] py-[0.75rem] fixed backdrop-blur-xs">
       <h1 className="font-syne text-1xl font-black cursor-pointer">
         JH<span className="text-red-600">.</span>
       </h1>
